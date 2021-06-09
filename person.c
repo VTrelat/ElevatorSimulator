a267dce8ebb4071009c98b4366dc419a1a872a49
Vincent Trélat <vincent.trelat@free.fr>
2021-06-09 11:23:21 +0200

    person.c updated

--- person.c
+++ person.c
@@ -6,3 +6,9 @@ Person* createPerson(int src, int dest){
     out->src = src;
     out->dest = dest;
 }
+
+PersonList* insert(Person* p, PersonList* list){
+    PersonList *out = malloc(sizeof(PersonList));
+    out->person = p;
+    out->next = list;
+}
